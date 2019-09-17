59c775b996136fe5dc7b8801b5d188ff9f57a01c
Aly-Elgharabawy <ali.k.elgharabawy@gmail.com>
2019-09-17 08:51:54 +0200

    Added training loop, now NN goes through 1 epoch and obtains accuracy of ~75%

--- CNN.py
+++ CNN.py
@@ -1,11 +1,14 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
 
 import torchvision
 import torchvision.transforms as transforms
 
 torch.set_printoptions(linewidth=120)
+torch.set_grad_enabled(True)
 
 train_set = torchvision.datasets.FashionMNIST(
     root = './data/FashionMNIST',
@@ -25,11 +28,16 @@ class Network(nn.Module):
         self.out = nn.Linear(in_features = 60, out_features = 10)
 
     def forward(self,t):
-        t = F.relu(self.conv1(t))
-        t= F.max_pool2d(t, kernel_size = 2, stride = 2)
+      
+
+        t = F.relu(self.conv1(t)) #all <0 becomes 0
+        t= F.max_pool2d(t, kernel_size = 2, stride = 2) #represents each region as its max i.e. 2x2 region --> 1x1 max val
+
 
         t = F.relu(self.conv2(t))
         t= F.max_pool2d(t, kernel_size = 2, stride = 2)
+
+      
 
         t = F.relu(self.fc1(t.reshape(-1,192)))
         t = F.relu(self.fc2(t))
@@ -37,12 +45,35 @@ class Network(nn.Module):
 
         return t
 
+def get_num_correct(predictions,targets):
+    return predictions.argmax(dim=1).eq(targets).sum().item()
+
+
 network = Network()
 
-sample = next(iter(train_set)) 
-image, label = sample
-pred = network(image.unsqueeze(0)) # unsqueeze 0 ---> simulate batch size i.e. 3d to 4d
-print(pred)
+data_loader = torch.utils.data.DataLoader(train_set,batch_size=100) 
+optimizer = optim.Adam(network.parameters(),lr=0.01)
+
+total_loss = 0 
+total_correct = 0
+
+
+for batch in data_loader:
+
+    images,labels = batch #acquires training data and target from batch
+    preds = network(images) #Transforms input into output throughout NN layers
+    loss = F.cross_entropy(preds,labels) #Calculates cross entropy using predictions and labels 
+
+    optimizer.zero_grad() #resets grad to 0 as pytorch does not reset them automatically
+    loss.backward() #Calculates gradients through backward traversal of comp graph and adds them to previous gradient value
+    optimizer.step() #Proceeds with gradient descent now that gradient is calculated
+
+    total_loss += loss.item() #sums loss values throughout batches
+    total_correct += get_num_correct(preds,labels)#sums correct guesses throughout batches
+
+
+print("epoch   :",str(0), "total_correct: ",str(total_correct),"loss: ",str(total_loss))
+
 
 
 
